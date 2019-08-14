917f97015ab8332488ab70287fb6d0e4c84e0de7
azilemdey <azilem.v.d@gmail.com>
2019-08-13 23:24:37 -0500

    guarda en almacenamiento Local Storage

--- src/main.js
+++ src/main.js
@@ -4,31 +4,25 @@
 //myFunction();
 
 //Delimitar el tamaño del texto
-let contenido_textarea = "" 
-const num_caracteres_permitidos = 10;
 const textarea = document.getElementById("post");
 
 textarea.addEventListener('keyup',()=>{
  const num_caracteres = textarea.value.length
-   if (num_caracteres > num_caracteres_permitidos){ 
-      textarea.value = contenido_textarea 
-   }else{ 
-      contenido_textarea = textarea.value	
-   } 
-
-   if (num_caracteres >= num_caracteres_permitidos){ 
+   if (num_caracteres >= 151 ){ 
       textarea.style.color="#ff0000"; 
    }else{ 
       textarea.style.color="#000000"; 
-   } })
+   }})
 
    //guardar el texto en variable
-   const text = textarea.value;
-   const saveText = localStorage.setItem('saving',text);
-   console.log(saveText);
+   
 
-   //const saveBtn = document.getElementById('save-text');
-  // saveBtn.addEventListener('click',()=)
+   const saveBtn = document.getElementById('save-text');
+   saveBtn.addEventListener('click',()=>{
+      const text = textarea.value;
+      localStorage.setItem('saving',text);
+      textarea.disabled=true;
+   })
 
  
  
