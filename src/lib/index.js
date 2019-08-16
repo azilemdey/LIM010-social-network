1e9896703839f3a908e79c7f54d9ab8d500205a2
azilemdey <azilem.v.d@gmail.com>
2019-08-16 13:13:37 -0500

    persistencia de datos en Local Storage

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,11 @@
 // aqui exportaras las funciones que necesites
-export const myFunction = () => {
-  // aqui tu codigo
+// export const textarea1 = (posts) => {
+const textarea = document.getElementById('post')
+export const textarea1 = (posts) => {
+  if (posts.value.length > 150) {
+    alert('Por Favor la descripción no debe exceder de 150 caracteres');
+    posts.focus();
+    return false;
+  }
+  return true;
 };
