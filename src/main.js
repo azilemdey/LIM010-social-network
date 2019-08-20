bec2795817af3487b8ce6d65a2b55889dbef3de2
azilemdey <azilem.v.d@gmail.com>
2019-08-20 09:34:18 -0500

    agregando estilo a la segunda pagina

--- src/main.js
+++ src/main.js
@@ -7,15 +7,15 @@
 
 const inputWritePost = document.getElementById('post');
 const botonSavePost = document.getElementById('save-text');
-const editBtn = document.getElementById('edit-text');
-const deleteText = document.getElementById('delete-text');
 const printerPost=document.getElementById("printer-post");
 let arrPost = [];
 
 
 window.onload = () => {if(localStorage.getItem('post') !==null){const printer=JSON.parse(localStorage.getItem('post'));
 for(let i=0;i<printer.length;i++){
-printerPost.innerHTML += `<textArea cols="40" rows="5" readonly name="texto" maxlength="151" class="post" id=${i}>${printer[i]}</textArea><button id=${i}>x</button>`
+printerPost.innerHTML += `<textArea class = "template-posts"cols="40" rows="5"  width="70%" readonly name="texto" maxlength="151" class="post" id=${i}>${printer[i]}</textArea>
+<div class="btn-edit"><button type="button" id=${i}><img src="editar.png" class="pencil"></button>
+<button type="button" id=${i}  ><img src="borrar.png" class="pencil"></button></div>`
 }}};
 
 inputWritePost.addEventListener('keyup', () => {
@@ -43,48 +43,9 @@ botonSavePost.addEventListener('click', () => {
       arrPost.push(text);
       localStorage.setItem('post', JSON.stringify(arrPost));
       for(let i=0;i<arrPost.length;i++){
-      printerPost.innerHTML += `<div><textArea cols="40" rows="5" readonly name="texto" maxlength="151" class="post" id=${i}>${arrPost[i]}</textArea><button id=${i}>x</button></div>`
+      printerPost.innerHTML += `<textArea cols="40" rows="5" readonly name="texto" maxlength="151" class="post" id=${i}>${arrPost[i]}</textArea>`
       };
       inputWritePost.value = '';
       console.log(arrPost)
     }
 });
-
-
-editBtn.addEventListener('click', () => {
-  textarea.disabled = false;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-});
-// guardar el texto en variable
-
-//const printerPost=document.getElementById("printer-post");
-deleteText.addEventListener('click', () => {
-  alert('Estas seguro que quiere eliminar');
-localStorage.removeItem('saving');
-//for(let i=0;i<arrPost.length;i++){
-//printerPost.innerHTML += `<textArea cols="40" rows="5" readonly name="texto" maxlength="151" class="post" id=${i}>${arrPost[i]}</textArea><button id=${i}>x</button>`
-//}
-  // localStorage.removeItem('saving');
-});
-
-tinymce.init({
-  selector: 'textarea',
-  plugins: 'lists media',
-  toolbar: 'addcomment showcomments casechange checklist code formatpainter insertfile pageembed permanentpen',
-  toolbar_drawer: 'floating',
-  tinycomments_mode: 'embedded',
-  tinycomments_author: 'Author name'
-});
-
-
-
-
-
-// const arr = ['lunes','martes','miercoles','jueves','viernes','sabado'];
-// const arrObj = [{},{},{}];
-
-// localStorage.setItem('propiedad1', JSON.stringify(arr));
-// localStorage.setItem('propiedad2', JSON.stringify(arrObj));
-// console.log(typeof localStorage.getItem('propiedad1'));
-// console.log(localStorage.getItem(JSON.parse('propiedad2')))
-
-// console.log(JSON.parse(localStorage.getItem('propiedad1')));
