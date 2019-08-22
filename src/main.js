c913e48f873e5d4d7bb175f45da1e18f0fa8aadc
azilemdey <azilem.v.d@gmail.com>
2019-08-22 13:07:50 -0500

    imprime template

--- src/main.js
+++ src/main.js
@@ -10,12 +10,12 @@ const botonSavePost = document.getElementById('save-text');
 const printerPost=document.getElementById("printer-post");
 let arrPost = [];
 
-
 window.onload = () => {if(localStorage.getItem('post') !==null){const printer=JSON.parse(localStorage.getItem('post'));
 for(let i=0;i<printer.length;i++){
 printerPost.innerHTML += `<textArea class = "template-posts"cols="40" rows="5"  width="70%" readonly name="texto" maxlength="151" class="post" id=${i}>${printer[i]}</textArea>
 <div class="btn-edit"><button type="button" id=${i}><img src="editar.png" class="pencil"></button>
-<button type="button" id=${i}  ><img src="borrar.png" class="pencil"></button></div>`
+<button type="button" id=${i}  ><img src="borrar.png" class="pencil"></button></div>`;
+inputWritePost.value = '';
 }}};
 
 inputWritePost.addEventListener('keyup', () => {
@@ -30,22 +30,26 @@ inputWritePost.addEventListener('keyup', () => {
 botonSavePost.addEventListener('click', () => {
   const text = inputWritePost.value;
 
-  if(localStorage.getItem('post') !== null ) {
-    const text1 = JSON.parse(localStorage.getItem('post'));
-    arrPost=text1.slice();
+  if(localStorage.getItem('post') !== null && text !== '' ) {
+    const arrPostLocalStorage = JSON.parse(localStorage.getItem('post'));
+    arrPost=arrPostLocalStorage.slice();
     arrPost.push(text);
-    //arr3.push(text); 
-    localStorage.setItem('post', JSON.stringify(arrPost));
-    console.log(arrPost)
-    //console.log(arr3);
-      }
+    localStorage.setItem('post', JSON.stringify(arrPost))
+    printerPost.innerHTML='';
+    for(let i=0;i<arrPostLocalStorage.length;i++){
+    printerPost.innerHTML += `<textArea class = "template-posts"cols="40" rows="5"  width="70%" readonly name="texto" maxlength="151" class="post" id=${i}>${arrPostLocalStorage[i]}</textArea>
+    <div class="btn-edit"><button type="button" id=${i}><img src="editar.png" class="pencil"></button>
+    <button type="button" id=${i}  ><img src="borrar.png" class="pencil"></button></div>`;
+    };
+  }
   else if(text !== ''){
       arrPost.push(text);
       localStorage.setItem('post', JSON.stringify(arrPost));
-      for(let i=0;i<arrPost.length;i++){
-      printerPost.innerHTML += `<textArea cols="40" rows="5" readonly name="texto" maxlength="151" class="post" id=${i}>${arrPost[i]}</textArea>`
+      printerPost.innerHTML = `<textArea class = "template-posts"cols="40" rows="5"  width="70%" readonly name="texto" maxlength="151" class="post" id=${0}>${arrPost[0]}</textArea>
+      <div class="btn-edit"><button type="button" id=${0}><img src="editar.png" class="pencil"></button>
+      <button type="button" id=${0}  ><img src="borrar.png" class="pencil"></button>`;
+  
       };
       inputWritePost.value = '';
-      console.log(arrPost)
-    }
 });
+
