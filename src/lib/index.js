c4194df8c6d820fb4c9aa23a30dcff581a312689
azilemdey <azilem.v.d@gmail.com>
2019-09-03 15:10:18 -0500

    merging

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,1 @@
 // aqui exportaras las funciones que necesites
-// export const textarea1 = (posts) => {
-const textarea = document.getElementById('post')
-export const textarea1 = (posts) => {
-  if (posts.value.length > 150) {
-    alert('Por Favor la descripción no debe exceder de 150 caracteres');
-    posts.focus();
-    return false;
-  }
-  return true;
-};
